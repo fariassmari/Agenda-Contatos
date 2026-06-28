c0fc766ed024f61a28569e8c846f843f648aecfa
Luca Lopes <77108115+LucaMarques@users.noreply.github.com>
2026-06-28 20:59:34 -0300

    Update ContatoPessoal.java

--- src/modelo_negocio/ContatoPessoal.java
+++ src/modelo_negocio/ContatoPessoal.java
@@ -8,9 +8,20 @@ import jakarta.persistence.PrimaryKeyJoinColumn;
 @Entity
 @PrimaryKeyJoinColumn(name = "id")
 public class ContatoPessoal extends Contato {
+    private int grauProximidade;
+
     public ContatoPessoal() {}
 
-    public ContatoPessoal(String nome, List<String> telefones, Cidade cidade) {
+    public ContatoPessoal(String nome, List<String> telefones, Cidade cidade, int grauProximidade) {
         super(nome, telefones, cidade);
+        this.grauProximidade = grauProximidade;
+    }
+
+    public int getGrauProximidade() {
+        return grauProximidade;
+    }
+
+    public void setGrauProximidade(int grauProximidade) {
+        this.grauProximidade = grauProximidade;
     }
 }
